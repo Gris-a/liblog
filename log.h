8b68f6b3764242e70e04f77c035b8a8c50ed56db
Gris-a <zavialov.ga@phystech.edu>
2023-11-17 22:17:06 +0300

    updated

--- log.h
+++ log.h
@@ -1,30 +1,20 @@
 #ifndef LOG_H
 #define LOG_H
 
-/**
- * @file log.h
- * @author GraY
- * @brief Functions for logging.
- */
-
 #include <stdio.h>
 #include <stdbool.h>
 
 #define PROTECT
 
 #ifndef LOG_CPP
-extern FILE *LOG_FILE; //< Automaticly externs log-file when included.
+extern FILE *LOG_FILE;
 #endif
 
 #define LOG(...) fprintf(LOG_FILE, __VA_ARGS__)
 #define LOGS(string) fputs(string, LOG_FILE)
 
 #ifdef PROTECT
-/**
- * @brief ifdef PROTECT checks condition and writes result into log-file.
- */
-#define ASSERT(condition, action)   if(!(condition))\
-                                    {\
+#define ASSERT(condition, action)   if(!(condition)) {\
                                         LOG ("%s:%s:%d: Assertion cathced at ASSERT(", __FILE__, __PRETTY_FUNCTION__, __LINE__);\
                                         LOGS(#condition);\
                                         LOG (", ...);\n");\
@@ -32,21 +22,13 @@ extern FILE *LOG_FILE; //< Automaticly externs log-file when included.
                                         action;\
                                     }
 #else
-#define ASSERT(...)
+#define ASSERT(condition, action)
 #endif
 
 #define EXEC_ASSERT(condition, action) {bool __temp__ = (condition); ASSERT(__temp__, action);}
 
-/**
- * @brief Opens log-file in "wb" mode with no buffering.
- * @return FILE* pointer to FILE for logging.
- * returns stderr if can`t open log-file.
- */
 FILE *open_log(void);
 
-/**
- * @brief Called automaticly by atexit() for log-file closing.
- */
 void close_log(void);
 
 #endif //LOG_H
